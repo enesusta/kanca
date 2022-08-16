d3f0d2d794983298f088ead3e92363343430fc80
Enes Usta <enesusta@protonmail.com>
2022-08-16 21:37:31 +0300

    KNC-28 feat: add delayed hook to increase performance

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as useAuthFetch } from './http/useAuthFetch';
-export { default as useFetch } from './http/useFetch';
-export { default as useDocumentTitle } from './util/useDocumentTitle';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {default as useAuthFetch} from './http/useAuthFetch';
 export {default as useFetch} from './http/useFetch';
 export {default as useDocumentTitle} from './util/useDocumentTitle';
+export {default as useDelayedState} from './useDelayedState';
