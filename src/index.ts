c4fa30f55e18781b94f9702ec8aa263655984d06
Enes Usta <enesusta@protonmail.com>
2023-03-05 01:16:32 +0300

    KNC-33 feat: debounced calback hook

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,15 @@ import {
   HttpInterface,
 } from "./http";
 
+import useDebouncedCallback, { DEBOUNCE_TYPE } from "./useDebouncedCallback";
+
 export {
+  useDebouncedCallback,
   useDocumentTitle,
   useHttpGet,
   useHttpPatch,
   useHttpPost,
   useHttpPut,
   HttpInterface,
+  DEBOUNCE_TYPE
 };
